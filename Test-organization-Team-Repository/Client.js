a08c43a6cb95674f816a5ecf2db72704e96b2e81
Dayana1407 <dayana@tvarana.com>
2015-08-24 22:26:02 +0530

    Commited By Priyanka 10:25pm

--- Test-organization-Team-Repository/Client.js
+++ Test-organization-Team-Repository/Client.js
@@ -1,16 +1,3 @@
-/**
- * Module Description
- * 
- * Version    Date            Author           Remarks
- * 1.00       20 Jun 2014     Priyanakkaannemadugu
- *
- */
-
-/**
- * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
- * @appliedtorecord recordType 
- * 
- * @param {String} type Access mode: create, copy, edit
- * @returns {Void}
- */
-/* testing */
+/* testing *//* testing *//* testing *//* testing *//* testing */
+/* testing *//* testing *//* testing *//* testing *//* testing */
+/* testing *//* testing *//* testing *//* testing *//* testing */
